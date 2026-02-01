e8de04954fe19b5b687db949fe417b2138eb2d17
KUMAR ABHINASH <abhiraj66369@gmail.com>
2026-02-01 15:07:16 +0530

    Backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -666,10 +666,12 @@ app.listen(5000, () => {
 
 
 /* ================= SERVER ================= */
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
-});
+// app.listen(5000, () => {
+//   console.log("🚀 Server running on http://localhost:5000");
+// });
 
-  app.listen(process.env.PORT || 5000, () => {
-    console.log("Server running");
-  });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
